8cdc3b00ce01e3358b9f14fcca3a322406e7639b
muradmecidov <muradmecidov1499@gmail.com>
2023-04-09 23:16:42 +0400

    task5

---
+++ task5.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string words = Console.ReadLine();
+
+
+            string corrected = Corrected(words);
+            Console.WriteLine(corrected);
+
+           Console.ReadLine();
+
+
+           
+
+        }
+
+
+        public static string Corrected(string words)
+        {
+            words = words.Trim(); 
+            string[] words2 = words.Split(' '); 
+
+            for (int i = 0; i < words2.Length; i++)
+            {
+                if (words2[i].Length > 0)
+                {
+                    words2[i] = words2[i].ToUpper() + words2[i].ToLower();
+                }
+            }
+
+            words = string.Join(" ", words2);
+
+            return words;
+        }
+    }
+}
